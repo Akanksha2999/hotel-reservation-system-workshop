30915701709a4e7d0b677661cec8150f9805d3b4
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-28 08:39:31 +0530

    ADDED: Added special rates for reward customers

--- java/com/bridgelabz/HotelReservationSystem.java
+++ java/com/bridgelabz/HotelReservationSystem.java
@@ -4,25 +4,37 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HotelReservationSystem {
     static List<Hotels> hotelsList = new ArrayList<>();
+    static LinkedList<Hotels> specialHotelList = new LinkedList<>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System Project");
-        Hotels lakewood = new Hotels("Lakewood", 110, 90,200, 3);
-        Hotels bridgewood = new Hotels("Bridgewood", 150, 50,200, 4);
-        Hotels ridgewood = new Hotels("Ridgewood", 220, 150,370, 5);
+        System.out.println("Rates for normal customers are: ");
+        Hotels lakewood = new Hotels("Lakewood", 110, 90, 200, 3);
+        Hotels bridgewood = new Hotels("Bridgewood", 150, 50, 200, 4);
+        Hotels ridgewood = new Hotels("Ridgewood", 220, 150, 370, 5);
         hotelsList.add(lakewood);
         hotelsList.add(bridgewood);
         hotelsList.add(ridgewood);
         hotelsList.forEach(System.out::println);
         System.out.println(" ");
+        System.out.println("Special rates for reward customers are: ");
+        Hotels lakewoodReward = new Hotels("Lakewood", 80, 80, 160, 3);
+        Hotels bridgewoodReward = new Hotels("Bridgewood", 110, 50, 160, 4);
+        Hotels ridgewoodReward = new Hotels("Ridgewood", 100, 40, 150, 5);
+        specialHotelList.add(lakewoodReward);
+        specialHotelList.add(bridgewoodReward);
+        specialHotelList.add(ridgewoodReward);
+        specialHotelList.forEach(System.out::println);
         cheapestHotelForWeekdays("2020-09-10", "2020-09-11");
         cheapestHotelForWeekend("2020-09-11", "2020-09-12");
         cheapestBestRatedHotel("2020-09-11", "2020-09-12");
         bestRatedHotel("2020-09-11", "2020-09-12");
+        System.out.println(" ");
     }
 
     public static void cheapestHotelForWeekdays(String startDate, String endDate) {
